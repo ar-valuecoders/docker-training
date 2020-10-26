51979f895ac740a532265dfa9be6d67099aba7e0
abhishek rawat <toabhishekrawat@gmail.com>
2020-10-26 19:25:29 +0530

    initial commit

---
+++ MyASPCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyASPCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
